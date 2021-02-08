ea74e77d3a06932f27576c9679780748d4a4cd07
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-02-08 03:38:24 -0600

    confirm delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,9 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    if user_signed_in? && current_user == @product.user && !params.has_key?(:client)
+      render :admin
+    end
   end
 
   # GET /products/new
