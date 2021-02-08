aefdf6efce6d1cb6c6e08edc639478160914a1a6
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-02-08 02:52:30 -0600

    avatar

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user
   validates_presence_of :name, :user, :pricing
   validates :pricing, numericality: { greater_than: 0}
+
+  has_attached_file :avatar, styles: { medium: "300x300",thumb:"100x100"},default_url: "missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/ 
+
 end
