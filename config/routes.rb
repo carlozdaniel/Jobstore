6c714870ad1df48c36b165cf26b4530d15decb4e
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-02-18 02:09:19 -0600

    styles nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :attachments, only: [:create, :destroy,:new,:show]
   resources :products
   resources :in_shopping_carts, only: [:create, :destroy]
+  devise_for :users
 
   get 'email/create'
-  devise_for :users
+ # post 'emails/create', as: :create_email
+  get "/carrito", to: "shopping_carts#show"
 
   post "email/create", as: :create_email
   authenticated :user do
